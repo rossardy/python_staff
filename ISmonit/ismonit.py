5d7a9ad1ac1270df79e179d957a2763be565465b
Rostyslav Yatsyshyn <rossardy@gmail.com>
2017-12-26 15:26:19 +0200

    improved show_list function and added some exceptions

--- ISmonit/ismonit.py
+++ ISmonit/ismonit.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python2.7
 # -*- coding: utf8 -*-
 
-import argparse, re, MySQLdb
+# This utility designed by Yatsyshyn Rostyslav
+# for internal L3 team using
+
+import argparse, re, MySQLdb, subprocess, socket, sys, os, errno
 
 from tabulate import tabulate
 
-DB_CONF_PATH = '/onapp/interface/config/database.yml'
-#DB_CONF_PATH = '/home/rossardy/src/python/file'
+# version of ismonit utility
+__version__ = "0.3.2"
 
-#Colours & styles
+# Configuration default files
+DB_CONF_PATH = '/onapp/interface/config/database.yml'
+ONAPP_CONF_PATH = '/onapp/interface/config/on_app.yml'
+
+# Colours & styles
 CYAN = '\033[36m'           # cyan
 PURPLE = '\033[35m'         # purple
 BLUE = '\033[34m'           # blue
@@ -66,11 +73,71 @@ class HV(BS):
         self.disks = disks
 
     def controlers_count(self):
+        """
+        This method count amount of storage controllers on particular HV
+        :return: amount of Storage Controllers
+        """
         count = self.disks // self.disk_per_controller
         rest = self.disks % self.disk_per_controller
         if rest != 0:
             count += 1
         return count
+
+def show_header():
+    """
+    Print welcome header in the top of terminal
+    """
+    print BOLD+YELLOW+'ISmonit is utility designed for checking Integrated Storage'+END
+    print YELLOW+'NOTE: Can be issues with checking cloud due isd/groupmon differences'+END
+
+    def chck_version():
+        """
+        It checks CP and Storage version and if IS is enabled
+        :return: versions of storage and Control Panel
+        """
+        cp_ver = stor_ver = 'not installed'
+        try:
+            cp_ver = re.findall(r"[0-9].{4,7}",
+                                subprocess.check_output(['bash', '-c', 'rpm -qa | grep onapp-cp-[345]']).rstrip('\n'))
+            stor_ver = re.findall(r"[0-9].{4,7}",
+                                  subprocess.check_output(['bash', '-c', 'rpm -qa | grep onapp-store-install']).rstrip(
+                                      '\n'))
+            if not os.path.exists('/onapp/interface/config/on_app.yml'):
+                raise IOError(" file doesn't exist")
+            elif not os.access(ONAPP_CONF_PATH, os.R_OK):
+                raise IOError(" file has no read permissions for current user")
+            is_enabled = subprocess.check_output(['bash', '-c','grep ^storage_enabled: /onapp/interface/config/on_app.yml | sed \'s/^storage_enabled: //g\'']).rstrip('\n')
+            if not is_enabled == 'true':
+                raise NameError('Integrated Storage isn\'t enabled')
+        except subprocess.CalledProcessError:
+            if cp_ver == 'not installed':
+                print RED + BOLD + 'WARNING: ', ' Control Panel packet isn\'t installed' + END
+                sys.exit(1)
+            elif stor_ver == 'not installed':
+                print RED + BOLD + 'WARNING: ', ' Storage packet isn\'t installed' + END
+                sys.exit(1)
+            else:
+                print RED + BOLD + 'Unknown error' + END
+        except NameError as e:
+            print RED + BOLD + 'WARNING: ', str(e) + END
+        except IOError as e:
+            print RED + BOLD + 'WARNING: ', ONAPP_CONF_PATH, str(e) + END
+            sys.exit(1)
+        count = 0
+        for i, j in zip(re.findall(r"[0-9]{1,}", cp_ver[0]), re.findall(r"[0-9]{1,}", stor_ver[0])):
+            if i == j:
+                count += 1
+            else:
+                break
+        if count == 0:
+            print RED + BOLD + 'WARNING: ' + END, 'Different major versions.'
+        elif count == 1:
+            print RED + BOLD + 'WARNING: ' + END, 'Different minor versions.'
+        return cp_ver, stor_ver
+    cp_ver, stor_ver = chck_version()
+    print CYAN+('Storage version is: {}'.format(stor_ver[0]))+END
+    print CYAN+('Control Penel version is: {}'.format(cp_ver[0]))+END
+    print " "
 
 def show_list():
     """
@@ -78,10 +145,7 @@ def show_list():
     """
     lst = bss_list + hvs_list
     on_line, off_line = [],[]
-    print '\n'
-    print BOLD+RED+'ISmonit is checking cloud which running on $version of storage'+END
-    print YELLOW+'NOTE: Can be issues with checking cloud due isd/groupmon differences'+END
-    print '\n'
+    show_header()
     on_line.append(['ONLINE:','','','','','',''])
     off_line.append(['OFFLINE:','','','','','',''])
     for i in lst:
@@ -99,19 +163,42 @@ def show_ips():
     This function prints ips all HVs and BSs in 'for' cycle format
     """
     lst = bss_list + hvs_list
-    hvs_ips = []
-    print '\n'
-    print BOLD+RED+'ISmonit is checking cloud which running on $version of storage'+END
-    print YELLOW+'NOTE: Can be issues with checking cloud due isd/groupmon differences'+END
-    print '\n'
+    hvs_ips,hvs_end,scs_end = [],[],[]
+    # Print welcome header in the top
+    show_header()
     for i in lst:
         if i.online == 1:
             hvs_ips.append(i.ip_address.split('.'))
-    tmp = '{' + hvs_ips[0][3]
-    for i in range(1,len(hvs_ips)):
-        tmp = tmp + ',' + hvs_ips[i][3]
-    tmp = tmp + '}'
-    print ('for i in {}.{}.{}.{}; do echo $i; ssh root@$i uptime; done'.format(hvs_ips[0][0],hvs_ips[0][1],hvs_ips[0][2],tmp))
+    # get last oct from each HV and BS
+    for i in range(0,len(hvs_ips)):
+        hvs_end.append(hvs_ips[i][3])
+    hvs_end.sort()
+    # convert to string
+    crs_string = "{" + ",".join(hvs_end) + "}"
+    # Print mangement IPs of Hvs and BSs
+    print YELLOW+BOLD+'From CP:'+END
+    print (YELLOW+ 'Get all CRs :'+END + 'for i in {}.{}.{}.{}; do echo $i; ssh root@$i uptime; done\n'.format(hvs_ips[0][0],hvs_ips[0][1],hvs_ips[0][2],crs_string))
+    print (YELLOW+ 'Copy to CRs :'+END + 'for i in {}.{}.{}.{}; do echo $i; scp /tmp/ root@$i:/tmp ; done\n'.format(hvs_ips[0][0],hvs_ips[0][1],hvs_ips[0][2],crs_string))
+    # get last oct for SCs
+    for i in hvs_list:
+        # Checking if there are some ips of controllers
+        if i.online == 1 and i.controlers_count() >= 1:
+            for j in range(1,i.controlers_count()+1):
+                scs_end.append(str(i.host_id)+ '.' + str(j))
+    scs_end.sort()
+    scs_string = "{" + ",".join(scs_end) + "}"
+    if len(scs_end) > 0:
+        if len(bss_list) > 0:
+            tmp = bss_list[0].ip_address
+        else:
+            tmp = hvs_list[0].ip_address
+        print (YELLOW+ 'Get all SCs :'+END + 'for i in 10.200.{}; do echo $i; ssh -tt root@{} -tt ssh $ssh_key $i uptime; done\n'.format(scs_string,tmp))
+        print (YELLOW+ 'Copy to SCs :'+END + 'for i in 10.200.{}; do echo $i; scp -i ~/.ssh/id_rsa.pub $ssh_key -o ProxyCommand="ssh  -W %h:%p {}" /tmp/ $i:/tmp/; done\n'.format(scs_string,tmp))
+        print YELLOW+BOLD+'From HV:'+END
+        print (YELLOW+ 'Get all SCs :'+END +  'for i in 10.200.{}; do echo $i; ssh $ssh_key root@$i uptime; done\n'.format(scs_string))
+        print ("ssh_key='-o ConnectTimeout=5 -o  ConnectionAttempts=1 -o PasswordAuthentication=no -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet -o CheckHostIP=no'\n")
+    else:
+        print RED+BOLD+'WARNING: '+END,'No one Storage Controller is avaialble on whole cloud!\n'
 
 def hvs_gener(crs,bss):
     #hvs_list = ['hv'+str(x) for x in range(1,len(hvs)+1)]
@@ -133,23 +220,47 @@ def hvs_gener(crs,bss):
     return None
 
 def parse_file():
-    with open(DB_CONF_PATH, "r") as dbfile:
-        for line in dbfile:
-            result = re.findall(r'\S*\S',line)
-            if result[0] == 'database:':
-                database = result[1]
-            if result[0] == 'host:':
-                host = result[1]
-            if result[0] == 'password:':
-                password = result[1]
-            if result[0] == 'port:':
-                port = result[1]
-    def parse_db(database,  host,  password, port):
-        db = MySQLdb.connect(host=host,               # host, usually localhost
-                             #port=port,               # port, usually 3306
-                             user="root",             # username, usually root
-                             passwd=password,         # password
-                             db=database)             # name of the data base
+    '''
+    This procedure parses yml file
+    :return:
+    '''
+    #raise exections when yml file doesn't exist or no read permissions or has no full range of data
+    try:
+        if not os.path.exists(DB_CONF_PATH):
+            raise IOError(" file doesn't exist")
+        elif not os.access(DB_CONF_PATH, os.R_OK):
+            raise IOError(" file has no read permissions for current user")
+        with open(DB_CONF_PATH, "r") as dbfile:
+            for line in dbfile:
+                result = re.findall(r'\S*\S',line)
+                if result[0] == 'database:':
+                    database = result[1]
+                if result[0] == 'host:':
+                    host = result[1]
+                if result[0] == 'password:':
+                    password = result[1]
+                if result[0] == 'port:':
+                    port = result[1]
+                if result[0] == 'username:':
+                    user = result[1]
+        if not database and host and password and port and user:
+            raise NameError()
+    except IOError as e:
+        print RED + BOLD + 'WARNING: ', DB_CONF_PATH, str(e) + END
+        sys.exit(1)
+    except NameError as e:
+        print RED + BOLD + 'WARNING: ', str(e) + END
+        sys.exit(1)
+    def parse_db(password, host='localhost', port=3306, user='root', database='onapp'):
+        try:
+            db = MySQLdb.connect(host=host,               # host, usually localhost
+                                 port=int(port),          # port, usually 3306
+                                 user=user,               # username, usually root
+                                 passwd=password,         # password
+                                 db=database)             # name of the data base, usually onapp
+        except MySQLdb.OperationalError as e:
+            print RED + BOLD + 'WARNING: ', str(e) + END
+            sys.exit(1)
         sql = db.cursor()
         sql.execute("select hypervisors.label, "
                     "hypervisors.ip_address, "
@@ -164,7 +275,8 @@ def parse_file():
                     "count(*) as disks from hypervisor_devices "
                     "left join hypervisors "
                     "on (hypervisor_devices.hypervisor_id = hypervisors.id) "
-                    "where type = 'Hypervisor::DiskDevice' and status = 1 group by hypervisor_id")
+                    "where type = 'Hypervisor::DiskDevice' and status = 1 and hypervisors.mac is not Null "
+                    "group by hypervisor_id")
         crs = tuple(sql)
         sql.execute("select label, "
                     "ip_address, "
@@ -174,19 +286,48 @@ def parse_file():
                     "host_id, "
                     "mtu, os_version, "
                     "hypervisor_type from hypervisors "
-                    "where host_id is not Null and backup = 1")
+                    "where host_id is not Null and mac is not Null and backup = 1")
         bss = tuple(sql)
         db.close()
-        #print crs
         hvs_gener(crs,bss)
-    crs = parse_db(database, host, password, port)
-    #print crs
+    crs = parse_db(password, host, port, user, database)
+
+def ping_network(zone=None):
+    """
+    Ping check Hvs/BSs with mtu.
+    :param zone:
+    :return: None
+    """
+    show_header()
+    lst = bss_list + hvs_list
+    for i in lst:
+        print (BOLD + YELLOW + '{} : {}  host_id={}'.format(i.label, i.ip_address, i.host_id)) + END
+        try:
+            output = subprocess.check_output(['bash','-c', ('ssh root@{} "onappstore nodes | grep ACTIVE -c"'.format(i.ip_address))]).rstrip('\n')
+        except subprocess.CalledProcessError:
+            output = '0'
+        print 'Amount of Nodes: {}'.format(output)
+        for j in lst:
+            ping = 'ssh root@' + i.ip_address + ' \"ping -q -c 1000 -i 0.001 -M do -s ' + str(i.mtu - 28) + ' ' + j.frontend_ip() + '\"'
+            ping_output = subprocess.check_output(['bash','-c', ping])
+            result = re.findall(r"[0-9]*\%",ping_output)
+            if int(result[0][:-1]) == 100:
+                print 'All packets are missed to ' , j.frontend_ip()
+            elif int(result[0][:-1]) > 0:
+                print 'WARNING: ' + 'Missed ' + result + ' packets to ', j.frontend_ip()
+
+#def omping(zone=None):
+   # show_header()
+   # lst = bss_list + hvs_list
+   # for i in lst:
+
+#ssh 10.76.0.23 "omping -q -c 100  10.200.5.254 10.200.4.254 10.200.3.254"
 
 if __name__ == '__main__':
     parse_file()
     # Use nargs to specify how many arguments an option should take.
     parser = argparse.ArgumentParser(description='ISmonit utility')
-    parser.add_argument('--version', action='version', version='%(prog)s 0.1')
+    parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
     parser.add_argument('--ips',  action='store_true')
     parser.add_argument('--list', action='store_true')
     parser.add_argument("--verbose","-v",action="store_true", help="Full description", default=False)
@@ -194,5 +335,7 @@ if __name__ == '__main__':
     opts = parser.parse_args()
     if opts.ips:
         show_ips()
-    if opts.list:
+    elif opts.list:
         show_list()
+    else:
+        ping_network()
